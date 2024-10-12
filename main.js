1fe4ae3dbe92683486d202b6fb989b96dbbbbc78
linling26 <ruolinlingjie@gmail.com>
2024-10-12 20:07:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-            console.log('ServiceWorker registered:', registration);
+            console.log('ServiceWorker registered:', registration.scope);
         }).catch((error) => {
             console.log('ServiceWorker registration failed:', error);
         });
