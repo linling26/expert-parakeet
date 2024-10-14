c008cb1af0cc5786ee13ea9557aaa525eeffceb3
linling26 <ruolinlingjie@gmail.com>
2024-10-15 00:12:45 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,21 +5,11 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/main.js'
+                '/expert-parakeet',
+                '/expert-parakeet/index.html',
+                '/expert-parakeet/style.css',
+                '/expert-parakeet/main.js'
             ]);
-        })
-    );
-});
-
-// キャッシュからリソースを取得
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-            );
         })
     );
 });
