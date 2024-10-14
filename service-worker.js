cfc99d23f1e8b3f55363c4e091594a851affb779
linling26 <ruolinlingjie@gmail.com>
2024-10-15 00:24:56 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -28,3 +28,11 @@ self.addEventListener('activate', (event) => {
         })
     );
 });
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
